abce48f2c7f7e2f3215211c2c922b4a3222d2bca
mpgsa <mpgsa@Sire-Auron-10>
2018-04-07 17:18:09 -0300

    I implemented the good practice and I finish the rental and add customers and movies

--- src/mainPack/FilmClub.java
+++ src/mainPack/FilmClub.java
@@ -3,23 +3,23 @@ package mainPack;
 import datas.*;
 import java.sql.Date;
 import java.time.LocalDate;
+import rentShop.RentShop;
 
 
 public class FilmClub
 {
-
-    
     public static void main(String[] args)
     {
         //DAO<Film> filmDao = new FilmDAO();
         
-        /*listFilm(filmDao);
+        /*
+        listFilm(filmDao);
         
         Film film = new  Film(0,"lalalalala", "1998-08-25", "It's good");
         
         filmDao.create(film);
         
-        listFilm(filmDao);*/
+        listFilm(filmDao);
         int id_client = 1;
         int id_film = 3;
         long rent_days = 2;
@@ -29,7 +29,7 @@ public class FilmClub
         LocalDate localDate_rent = LocalDate.now();
         LocalDate localDate_dev = LocalDate.now().plusDays(rent_days);
         
-        Date rent_date = Date.valueOf(localDate_rent); 
+        Date rent_date = Date.valueOf(localDate_rent);
         Date dev_date = Date.valueOf(localDate_dev);
         
         RentFilm rentFilm = new RentFilm(
@@ -38,14 +38,14 @@ public class FilmClub
                 rent_date, 
                 dev_date);
         
-        
         DAO<RentFilm> rentDao = new RentFilmDAO();
 
         rentDao.create(rentFilm);
         
         listFilm(rentDao);
         
-        
+        RentShop rentShop = new RentShop();
+        */
         //listFilm(rentDao); 
     }
     
--- src/rentShop/RentShop.java
+++ src/rentShop/RentShop.java
@@ -2,7 +2,31 @@
 package rentShop;
 
 
-public class RentShop
+import javax.swing.JFrame;
+
+
+public class RentShop extends JFrame
 {
+    private final int WIDTH_W = 600;
+    private final int HEIGHT_W = 700;
+    private final int X = 400;
+    private final int Y = 50;
+    
+
+    public RentShop()
+    {
+        setTitle("Film Club");
+        
+        setBounds(X, Y, WIDTH_W, HEIGHT_W);
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        setVisible(true);
+        
+         
+    }
+    
+    
+   
     
 }
